dafa24aef671b05b63a5b71cf47672e6dff7ee44
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-14 13:19:53 +0200

    Update countAllFromTown.tests.js

--- test/countAllFromTown.tests.js
+++ test/countAllFromTown.tests.js
@@ -10,6 +10,6 @@ describe ("countAllFromTown function", function(){
         assert.equal(1, countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CF'))
     });
     it("returns the number of registration numbers that only match the first code 'CA' with '2' ", function(){
-        assert.equal(3, countAllFromTown('CA 345 123, CK 345, CA 123', 'CA'))
+        assert.equal(2, countAllFromTown('CA 345 123, CK 345, CA 123', 'CA'))
     })
 })
