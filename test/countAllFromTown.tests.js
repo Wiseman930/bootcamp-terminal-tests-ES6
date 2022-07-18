1cf80d8e0caecfc79e7ad49b5603462b0baac1a3
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-18 12:02:56 +0200

    Update countAllFromTown.tests.js

--- test/countAllFromTown.tests.js
+++ test/countAllFromTown.tests.js
@@ -3,13 +3,13 @@ import assert from "assert";
 import countAllFromTown from "../countAllFromTown.js";
 
 describe ("countAllFromTown function", function(){
-    it("returns the number of registration numbers that only match the first code 'CL' with '3' ", function(){
+    it("returns the number of registration numbers that only belong to Stellenbosch ", function(){
         assert.equal(3, countAllFromTown('CL 124,CY 567,CL 345, CJ 456,CL 341','CL'))
     });
-    it("returns the number of registration numbers that only match the first code 'CF' with '2' ", function(){
-        assert.equal(1, countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CF'))
+    it("returns the number of registration numbers that only belong to paarl ", function(){
+        assert.equal(1, countAllFromTown('CJ 124,CY 567,CL 345, CF 456, CL 341','CJ'))
     });
-    it("returns the number of registration numbers that only match the first code 'CA' with '2' ", function(){
+    it("returns the number of registration numbers that only only belong to cape town ", function(){
         assert.equal(2, countAllFromTown('CA 345 123, CK 345, CA 123', 'CA'))
     })
 })
