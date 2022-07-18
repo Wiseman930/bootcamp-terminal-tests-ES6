58ce811f5ac07f74d257c0d91f907b013c51c51e
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-18 12:17:14 +0200

    Update regCheck.tests.js

--- test/regCheck.tests.js
+++ test/regCheck.tests.js
@@ -2,16 +2,16 @@ import assert from "assert";
 import regCheck from "../regCheck.js";
 
 describe ("the regCheck function", function(){
-    it("should return true if a registration number taken in is from Gauteng or Eastern Cape or Limpopo or Mpumalanga with 'true'", function(){
+    it("should return true if a registration number taken in is from Gauteng", function(){
         assert.equal(true, regCheck('DC 55 YU GP', 'GP'));
     });
-    it("should return true if a registration number taken in is from Gauteng or Eastern Cape or Limpopo or Mpumalanga with 'true'", function(){
+    it("should return true if a registration number taken in is from Limpopo", function(){
         assert.equal(true, regCheck('DC 55 YU L', 'L'));
     });
-    it("should return true if a registration number taken in is from Gauteng or Eastern Cape or Limpopo or Mpumalanga with 'true'", function(){
+    it("should true if a registration number taken in is from Mpumalanga", function(){
         assert.equal(true, regCheck('DC 55 YU MP', 'MP'));
     });
-    it("should return false if a registration number taken in is not from Gauteng or Eastern Cape or Limpopo or Mpumalanga with 'false'", function(){
+    it("should return false if a registration number taken in is not from Gauteng or Eastern Cape or Limpopo or Mpumalanga", function(){
         assert.equal(false, regCheck('CJ 123 908', 'CJ'));
     });
 })
