2132416028f3773177f19f0005813db7cdcf3e64
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-18 12:45:43 +0200

    Update countAllPaarl.tests.js

--- test/countAllPaarl.tests.js
+++ test/countAllPaarl.tests.js
@@ -2,13 +2,13 @@
 import assert from "assert";
 import countAllPaarl from "../countAllPaarl.js";
 describe ("countAllPaarl function", function(){
-    it("returns the number registration numbers that only belong to paarl with '3' ", function(){
+    it("returns the number of registration numbers that only belong to paarl", function(){
         assert.equal(3, countAllPaarl('CJ 345 123, CJ 2345, CL 123-546, CK 345, CJ 123'))
     });
-    it("returns the number registration numbers that only belong to paarl with '2' ", function(){
+    it("returns the number of registration numbers that only belong to paarl", function(){
         assert.equal(2, countAllPaarl('CJ 345 123, CK 345, CJ 123'))
     });
-    it("returns the number registration numbers that only belong to paarl with '0' ", function(){
+    it("returns 0 when there is no paarl registration number", function(){
         assert.equal(0, countAllPaarl('CA 345 123, CK 345, CA 123'))
     })
 })
