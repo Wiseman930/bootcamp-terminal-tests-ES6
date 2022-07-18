d55c39e7216bac94567bcaf3e5faf3aa20cd71ea
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-18 12:09:44 +0200

    Update totalPhoneBill.tests.js

--- test/totalPhoneBill.tests.js
+++ test/totalPhoneBill.tests.js
@@ -2,17 +2,17 @@ import assert from "assert";
 import totalPhoneBill from "../totalPhoneBill.js";
 
 describe ("totalPhoneBill function", function(){
-    it("returns the total amount R0.00 when there are no call or sms", function(){
+    it("returns the total amount R0.00 when there are no call or sms made", function(){
         assert.equal("R0.00", totalPhoneBill(''))
     });
-    it("returns the total amount R1.95 when there are 3 sms", function(){
+    it("returns the total amount R1.95 when there are 3 sms made", function(){
         assert.equal("R1.95", totalPhoneBill('sms,sms,sms'))
     });
 
-    it("returns the total amount R11.00 when there are 4 calls", function(){
+    it("returns the total amount R11.00 when there are 4 calls made", function(){
         assert.equal("R11.00", totalPhoneBill('call,call,call,call'))
     });
-    it("returns the total amount R12.30 when there are 4 calls and 2 sms", function(){
+    it("returns the total amount R12.30 when there are 4 calls and 2 sms made", function(){
         assert.equal("R12.30", totalPhoneBill('call,call,call,call,sms,sms'))
     });
 })
