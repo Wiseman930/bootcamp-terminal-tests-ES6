434608f5ac45bca36f99351e31ead6a5afefb9b7
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-07-18 12:08:06 +0200

    Update yearsArgo.tests.js

--- test/yearsArgo.tests.js
+++ test/yearsArgo.tests.js
@@ -2,13 +2,13 @@ import assert from "assert";
 import yearsAgo from "../yearsAgo.js";
 
 describe ("the yearsAgo function", function(){
-    it("should return the number years between the current year and 1976 with '46'", function(){
+    it("should return the number years between the current year and 1976", function(){
         assert.equal(46, yearsAgo(1976));
     })
-    it("should return the number years between the current year and 1976 with '22'", function(){
+    it("should return the number years between the current year and 1976", function(){
         assert.equal(22, yearsAgo(2000));
     })
-    it("should return the number years between the current year and 1997 with '25'", function(){
+    it("should return the number years between the current year and 1997", function(){
         assert.equal(25, yearsAgo(1997));
     })
 })
